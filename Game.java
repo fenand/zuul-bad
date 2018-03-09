b42b6a02249f44df4a3deead40347a5f478b1161
Fenand <garciaf4c@gmail.com>
2018-03-09 11:08:22 +0100

    Commit 03 - modificada la clase game para evistar la repeticion de codigo y tener mejor cohesion

--- Game.java
+++ Game.java
@@ -110,22 +110,7 @@ public class Game
         System.out.println("World of Zuul is a new, incredibly boring adventure game.");
         System.out.println("Type 'help' if you need help.");
         System.out.println();
-        // modificado el string You are antes de currentRoom.getDescription()
-        System.out.println("You are " + currentRoom.getDescription());
-        System.out.print("Exits: ");
-        if(currentRoom.northExit != null) {
-            System.out.print("north ");
-        }
-        if(currentRoom.eastExit != null) {
-            System.out.print("east ");
-        }
-        if(currentRoom.southExit != null) {
-            System.out.print("south ");
-        }
-        if(currentRoom.westExit != null) {
-            System.out.print("west ");
-        }
-        System.out.println();
+        printLocationInfo();
     }
 
     /**
@@ -206,22 +191,7 @@ public class Game
         }
         else {
             currentRoom = nextRoom;
-            // modificado el string You are antes de currentRoom.getDescription()
-            System.out.println("You are " + currentRoom.getDescription());
-            System.out.print("Exits: ");
-            if(currentRoom.northExit != null) {
-                System.out.print("north ");
-            }
-            if(currentRoom.eastExit != null) {
-                System.out.print("east ");
-            }
-            if(currentRoom.southExit != null) {
-                System.out.print("south ");
-            }
-            if(currentRoom.westExit != null) {
-                System.out.print("west ");
-            }
-            System.out.println();
+            printLocationInfo();
         }
     }
 
@@ -239,5 +209,28 @@ public class Game
         else {
             return true;  // signal that we want to quit
         }
+    }
+
+    /**
+     * nuevo metodo privado para evitar la repeticion de codigo entre printwelcome y goRoom
+     */
+    private void printLocationInfo(){
+        // modificado el string You are antes de currentRoom.getDescription()
+        System.out.println("You are " + currentRoom.getDescription());
+        System.out.print("Salidas: ");
+        if(currentRoom.northExit != null) {
+            System.out.print("north ");
+        }
+        if(currentRoom.eastExit != null) {
+            System.out.print("east ");
+        }
+        if(currentRoom.southExit != null) {
+            System.out.print("south ");
+        }
+        if(currentRoom.westExit != null) {
+            System.out.print("west ");
+        }
+        System.out.println();
+
     }
 }
