bdaf96a75be5628b6eb1c79e2c60dabb1efcb2af
fenand <you@example.com>
2018-03-26 13:55:05 +0200

    commit r01 -- comando take

--- Player.java
+++ Player.java
@@ -13,28 +13,14 @@ public class Player
     // creamos una coleccionen en forma de pila para has rooms
     private Stack<Room>rooms;
     //creamos un arraylist de los objetos que vamos a ir añadiendo a la mochilo
-    // private ArrayList<Item> mochilo;
+    private ArrayList<Item> mochilo;
 
     //Peso Maximo que puede llevar el jugador en una constante por eso va en mayusculas.
     // private static final float PESO_MAXIMO = 50;
 
     //peso que tiene en la mochilo actualmente
-    // private float pesoMochilo;
+    private float pesoMochilo;
 
-    // 1-Implementa la posibilidad de que el jugador pueda coger objetos sin tener en cuenta el peso del objeto. 
-    // Haz un commit dentro de la rama (commit r01). hecho--------------------
-
-    // 2-Implementa la posibilidad de que haya objetos en el juego que no se puedan coger. 
-    // Haz un commit dentro de la rama (commit r02).hecho --------------
-
-    // 3-Implementa un comando items que haga que se impriman por pantalla todos los objetos que lleva ahora mismo el jugador, 
-    // junto con su peso total. Haz un commit dentro de la rama (commit r03).hecho --------
-
-    // 4-Implementa la posibilidad de que el jugador pueda soltar objetos. 
-    // Haz un commit dentro de la rama (commit r04).hecho----
-
-    // 5-Implementa que el jugador pueda llevar objetos hasta un peso máximo especificado cuando se construye el jugador. 
-    // El peso máximo que cada jugador puede llevar es un atributo del jugador. Haz un commit dentro de la rama (commit r05)
 
     /**
      * Constructor for objects of class Player
@@ -44,8 +30,8 @@ public class Player
         //inicializamos la pila
         rooms = new Stack<>();
         // inicializamos el arraylist y el peso que tienes en la mochila
-        // mochilo = new ArrayList<>();
-        // pesoMochilo = 0;
+        mochilo = new ArrayList<>();
+        pesoMochilo = 0;
 
     }
 
@@ -103,6 +89,40 @@ public class Player
     public void printLocationInfo(){ 
         System.out.println(currentRoom.getLongDescription());  
     }
+    /**
+     * Metodo para añadir items al inventario
+     * @param un item que es un obejeto item
+     */
+    public void addItems(Item item){
+        mochilo.add(item);
+    }
 
-    
+    /**
+     * comando take para recoger un itme de la sala y guardarlo en la mochilo
+     */
+    public void take(Command command){
+        if(!command.hasSecondWord()){
+            System.out.println("Escribe el objeto que quieres guardar!!");
+            return;
+        }
+
+        String objetoACoger = command.getSecondWord();
+        if(currentRoom.getItem(objetoACoger) != null){
+            if (currentRoom.getCantidadDeItems() > 0) {
+                mochilo.add(currentRoom.getItem(objetoACoger));
+                pesoMochilo += currentRoom.getItem(objetoACoger).getItemWeight();
+                currentRoom.borrarItem(objetoACoger);
+                System.out.println("Has cogido este item: " + objetoACoger);
+            }
+            else{
+                System.out.println("La sala esta desierta, cambia de sala para encontrar algo!!");
+            }
+        }
+
+        else{
+            System.out.println("El objeto no existe!!");
+        }
+
+    }
+
 }
